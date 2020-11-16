9951a3279ea3631ad0d25e4d7a0d9fc2be860237
Mateusz <1705931@brunel.ac.uk>
2020-11-16 22:02:50 +0000

    Add option for selecting a repository in the GUI

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,16 +1,25 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MainGUI extends JFrame{
     private JButton analyseRepoButton;
     private JPanel rootPanel;
     private JComboBox languageSelector;
+    private JComboBox repoSelector;
+    String[] dirnames; //this array stores the names of directories
+    File f = new File("D:\\IntelliJ Projects\\VulinOSS\\vulinoss"); //path of where the directories
 
     public MainGUI()
     {
         //This uses the designer form
         add(rootPanel);
+
+        //for each directory name, add it to the repoSelector JComboBox
+        for (String s : dirnames = f.list()) {
+            repoSelector.addItem(s);
+        }
 
         setTitle("Repo Analyser");
         setSize(400, 500);
@@ -21,7 +30,7 @@ public class MainGUI extends JFrame{
             {
                 try{
                     if (languageSelector.getSelectedItem().equals("Python")){
-                        Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd ..\\VulinOSS\\vulinoss && pylint binutils\"");
+                        Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd ..\\VulinOSS\\vulinoss && pylint " + repoSelector.getSelectedItem());
                     }
                 } catch (Exception ex)
                 {
